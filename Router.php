6b2d3d94b12dfbb41752cd52b8f332917e9265d6
Julio-G-Marrero <58669766+Julio-G-Marrero@users.noreply.github.com>
2022-09-21 17:07:12 -0500

    Update Router.php

--- Router.php
+++ Router.php
@@ -7,28 +7,41 @@ class Router
     public array $getRoutes = [];
     public array $postRoutes = [];
 
-    public function get($url, $fn) {
+    public function get($url, $fn)
+    {
         $this->getRoutes[$url] = $fn;
     }
 
-    public function post($url, $fn) {
+    public function post($url, $fn)
+    {
         $this->postRoutes[$url] = $fn;
     }
 
-    public function comprobarRutas() {
-        $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' :  $_SERVER['REQUEST_URI'] ;
+    public function comprobarRutas()
+    {
+        
+        // Proteger Rutas...
+        session_start();
+
+        // Arreglo de rutas protegidas...
+        // $rutas_protegidas = ['/admin', '/propiedades/crear', '/propiedades/actualizar', '/propiedades/eliminar', '/vendedores/crear', '/vendedores/actualizar', '/vendedores/eliminar'];
+
+        // $auth = $_SESSION['login'] ?? null;
+
+        if (isset($_SERVER['PATH_INFO'])) {
+            $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
+        } else {
+            $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
+        }
+        
         $method = $_SERVER['REQUEST_METHOD'];
 
-         
-        //dividimos la URL actual cada vez que exista un '?' eso indica que se están pasando variables por la url
-        $splitURL = explode('?', $currentUrl);
-        // debuguear($splitURL);
-        
         if ($method === 'GET') {
-            $fn = $this->getRoutes[$splitURL[0]] ?? null; //$splitURL[0] contiene la URL sin variables 
+            $fn = $this->getRoutes[$currentUrl] ?? null;
         } else {
-            $fn = $this->postRoutes[$splitURL[0]] ?? null;
+            $fn = $this->postRoutes[$currentUrl] ?? null;
         }
+
 
         if ( $fn ) {
             // Call user fn va a llamar una función cuando no sabemos cual sera
@@ -38,7 +51,9 @@ class Router
         }
     }
 
-    public function render($view, $datos = []) {
+    public function render($view, $datos = [])
+    {
+
         // Leer lo que le pasamos  a la vista
         foreach ($datos as $key => $value) {
             $$key = $value;  // Doble signo de dolar significa: variable variable, básicamente nuestra variable sigue siendo la original, pero al asignarla a otra no la reescribe, mantiene su valor, de esta forma el nombre de la variable se asigna dinamicamente
